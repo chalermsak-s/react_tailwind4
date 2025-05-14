1ab0e28227fe151653831e06fb2cd81c1ff557d0
chalermsak-s <kaidohjung@gmail.com>
2025-05-14 15:25:33 +0700

    Learn Button ui TailwindCss

--- react-tailwind-vite/src/pages/Home.tsx
+++ react-tailwind-vite/src/pages/Home.tsx
@@ -1,8 +1,46 @@
 const Home = () => {
   return (
     <>
-    <h1 className="text-3xl">Home</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut ducimus vitae laborum praesentium? Aliquid incidunt repellat dolor quia doloribus. Consequatur nihil explicabo exercitationem suscipit qui minima quas sed delectus velit?</p>
+      {/* Utility Class in Tailwind Css */}
+      <div className=" flex items-center max-w-sm bg-white mx-auto rounded-xl shadow-lg outline-black/5 p-6 gap-x-4 dark:bg-slate-800 dark:shadow-none">
+        <img className="size-12" src="/images/chaticon.svg" alt="Logo App" />
+        <div>
+          <div className="text-xl font-bold text-black dark:text-white">Kuma Chat</div>
+          <p className="text-gray-500 dark:text-slate-400">You have a new message!</p>
+        </div>
+      </div>
+
+      {/* Create Button in Tailwind Css */}
+      <div className="flex flex-col items-center gap-4 p-6">
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-xs hover:bg-blue-600 cursor-pointer">Primary</button>
+        <button className="px-4 py-2 bg-gray-500 text-white rounded-xs hover:bg-gray-600 cursor-pointer">Secondary</button>
+        <button className="px-4 py-2 bg-red-500 text-white rounded-xs hover:bg-red-600 cursor-pointer">Danger</button>
+
+        {/* Create Button Gadient */}
+        <button className="px-4 py-2 bg-gradient-to-r from-green-400 to-blue-500 text-white rounded-xs hover:from-green-600 hover:to-blue-600 cursor-pointer">Gadient</button>
+
+        {/* Button with Shadow */}
+        <button className="px-4 py-2 bg-indigo-500 text-white rounded-xs hover:bg-indigo-600 cursor-pointer shadow-lg">Shadow Button</button>
+
+        {/* Button Rounded Full */}
+        <button className="px-4 py-2 bg-purple-500 text-white hover:bg-purple-600 cursor-pointer shadow-lg rounded-full">Rounded Button</button>
+
+        {/* Small */}
+        <button className="px-4 py-2 text-sm min-w-36 bg-blue-500 text-white rounded-xs hover:bg-blue-600 cursor-pointer">Small</button>
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-xs hover:bg-blue-600 cursor-pointer">Medium</button>
+        <button className="px-4 py-2 text-lg min-w-36 bg-blue-500 text-white rounded-xs hover:bg-blue-600 cursor-pointer">Large</button>
+
+        {/* Disabled */}
+        <button className="px-4 py-2 text-lg min-w-36 bg-blue-500 text-white rounded-xs opacity-50 cursor-not-allowed" disabled>Disabled</button>
+
+        {/* Button With Icon */}
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-xs hover:bg-blue-600 cursor-pointer flex items-center gap-2"><img className="size-5" src="/images/chaticon.svg" alt="Chat Icon" />Chat</button>
+
+        {/* Button Animation */}
+        <button className="px-4 py-2 text-sm min-w-36 bg-blue-500 text-white rounded-xs hover:bg-blue-600 cursor-pointer hover:scale-110 transition-transform">Small</button>
+      </div>
+
+
     </>
   )
 }
